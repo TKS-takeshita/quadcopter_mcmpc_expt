201b54dcca69d8d643266c85d1689fa4bb9f7178
TKS-takeshita <takeshita-k@roboken.iit.tsukuba.ac.jp>
2026-05-13 07:51:28 +0900

    syuuseigo log

--- quadcopter_mcmpc_position/include/quadcopter_mcmpc_position/const_params.hpp
+++ quadcopter_mcmpc_position/include/quadcopter_mcmpc_position/const_params.hpp
@@ -19,7 +19,7 @@
 #define _INV_SQRT_2		0.7071067811865475f // 1/sqrt(2)
 
 // const for device (GPU)
-#define _DEVICE_CONST_HORIZON 			100 // horizon
+#define _DEVICE_CONST_HORIZON 			50 // horizon
 #define _DEVICE_CONST_THREAD_PER_BLOCK 	128 //_DEVICE_CONST_THREAD_PER_BLOCK * N_OF_BLOCK = N_OF_SAMPLES
 #define _DEVICE_CONST_N_OF_BLOCK 		64
 
